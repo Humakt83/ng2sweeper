ac12884fd6036a196005085a97521afcc41331e0
Humakt83 <urlact@yahoo.com>
2016-04-19 22:19:09 +0300

    Add px to style rule

--- src/app/components/board.directive.ts
+++ src/app/components/board.directive.ts
@@ -4,7 +4,7 @@ import {Directive, ElementRef, Input, Renderer} from 'angular2/core';
 })
 export class BoardDirective {
     constructor(private el: ElementRef, private window: Window, private renderer: Renderer) {
-        let height = window.innerHeight;
+        let height = window.innerHeight + 'px';
         let native = el.nativeElement;
         renderer.setElementStyle(native, 'height', height);
         renderer.setElementStyle(native, 'maxHeight', height);
